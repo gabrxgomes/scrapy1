a37350ff00fe5acd3f44eb8d239be05ea7fcb494
Gabriel <gabrielnasciment123@hotmail.com>
2024-07-12 09:58:18 -0300

    Enviando arquivos iniciais

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+# URL do site que você quer fazer o scraping
+url = "https://www.dafiti.com.br/roupas-masculinas/camisas/"  # substitua pelo URL desejado
+
+# Fazendo a requisição HTTP para obter o conteúdo da página
+response = requests.get(url)
+content = response.text
+
+# Usando BeautifulSoup para analisar o HTML
+soup = BeautifulSoup(content, 'html.parser')
+text = soup.get_text()
+
+# Usando a expressão regular para buscar a palavra "camisa"
+resultados = re.findall(r"camisas", text, re.IGNORECASE)
+
+# Exibindo os resultados
+print(f"Encontramos {len(resultados)} ocorrências da palavra 'camisas'.")
+for resultado in resultados:
+    print(resultado)
